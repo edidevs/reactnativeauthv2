edb2d55bd6df0c3eec4fb75d9fd2088fea7f58b9
thejuniordevops <edigithub@gmail.com>
2019-02-21 22:03:48 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+
+    render(){
+
+        return (
+            <View>
+                <Text>An app</Text>
+            </View>
+        );
+
+
+    }
+
+
+
+}
+
+export default App;
